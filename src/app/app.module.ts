5ea8c7a32be2b851b68e16075c79b25aef3c3761
nekokanbaru <mihailorang@gmail.com>
2022-10-13 14:53:09 +0200

    add API logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountryComponent } from './country/country.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CountryComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CountryComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-country',
@@ -6,7 +8,115 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./country.component.css'],
 })
 export class CountryComponent implements OnInit {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {}
+  countries: any = [];
+  countriesArray: any = [];
+  error = false;
+  selectedRegion: string = 'Filter by Region';
+
+  ngOnInit() {
+    this.fetchCountries();
+  }
+
+  private async fetchCountries() {
+    this.error = false;
+    this.http
+      .get('https://restcountries.com/v3.1/all')
+      .pipe(
+        map((responseData: any) => {
+          const postsArray = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )
+      .subscribe((data) => {
+        this.countries = data;
+
+        let arrayOfNumbers: any = [];
+        while (arrayOfNumbers.length < 8) {
+          let number = Math.floor(Math.random() * this.countries.length);
+          if (arrayOfNumbers.indexOf(number) === -1) {
+            arrayOfNumbers.push(number);
+          }
+        }
+        for (let i = 0; i < arrayOfNumbers.length; i++) {
+          this.countriesArray.push(this.countries[arrayOfNumbers[i]]);
+        }
+      });
+  }
+
+  timeout: any;
+  countryName: string = '';
+
+  countrySearch() {
+    this.error = false;
+    clearTimeout(this.timeout);
+
+    this.timeout = setTimeout(() => {
+      this.countriesArray = []; //clear countries before displaying the searched ones
+
+      if (this.countryName == '') {
+        this.fetchCountries();
+      } else {
+        this.http
+          .get('https://restcountries.com/v3.1/name/' + this.countryName)
+          .pipe(
+            map((responseData: any) => {
+              const postsArray = [];
+              for (const key in responseData) {
+                if (responseData.hasOwnProperty(key)) {
+                  postsArray.push({ ...responseData[key], id: key });
+                }
+              }
+              return postsArray;
+            })
+          )
+          .subscribe(
+            (data) => {
+              this.countries = data;
+              for (let i = 0; i < this.countries.length; i++) {
+                this.countriesArray.push(this.countries[i]);
+              }
+            },
+            (error) => {
+              this.error = true;
+              this.countriesArray = []; //clear countries if error happens
+              this.countries = [];
+              console.log(error);
+            }
+          );
+      }
+    }, 1000);
+  }
+
+  regionFilter() {
+    //https://restcountries.com/v3.1/region/{region}
+    this.error = false;
+
+    this.countriesArray = []; //clear countries before displaying the searched ones
+    this.http
+      .get('https://restcountries.com/v3.1/region/' + this.selectedRegion)
+      .pipe(
+        map((responseData: any) => {
+          const postsArray = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )
+      .subscribe((data) => {
+        this.countries = data;
+        for (let i = 0; i < this.countries.length; i++) {
+          this.countriesArray.push(this.countries[i]);
+        }
+      });
+  }
 }
